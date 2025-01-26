0f3a9577d24aaa9ff44a35ebd1c4f33acd73392f
Marini23 <marina.smile1m@gmail.com>
2025-01-26 10:33:50 +0100

    add: style mobile mint section

--- src/components/Mint/Mint.tsx
+++ src/components/Mint/Mint.tsx
@@ -23,29 +23,40 @@ export const Mint: React.FC<MintProps> = ({ widthScreen }) => {
   } = useForm<FormData>();
   const onSubmit = handleSubmit((data) => console.log(data));
   return (
-    <section>
-      <h2>ARE YOU IN?</h2>
+    <section className="mint-section">
+      <h2 className="mint-title">ARE YOU IN?</h2>
       <Icon name="icon-add-sharp" size={iconSize} />
-      <p>
+      <p className="mint-text">
         Join the YACHT APE community to be one of the first to receive our
         limited edition NFT
       </p>
-      <form onSubmit={onSubmit}>
-        <div>
-          {" "}
-          <label>First Name</label>
-          <input {...register("userName")} />
+      <form onSubmit={onSubmit} className="form-wrapper">
+        <div className="input-container">
+          <label className="label-icon">
+            <Icon name="icon-icon-username" size={24} />
+          </label>
+          <input
+            {...register("userName")}
+            className="mint-input"
+            placeholder="@username"
+          />
         </div>
-        <div>
-          {" "}
-          <label>Last Name</label>
-          <input {...register("walletAddress")} />
+        <div className="input-container">
+          <label className="label-icon">
+            <Icon name="icon-icon-wallet" size={24} />
+          </label>
+          <input
+            {...register("walletAddress")}
+            className="mint-input"
+            placeholder="Wallet address"
+          />
         </div>
         <button
           type="button"
           onClick={() => {
             console.log("submit");
           }}
+          className="mint-button"
         >
           MINT
         </button>
